380c04efa3bc1fa85b29dbcf87f4c823fc064c08
Larissa <larissagomes8020@gmail.com>
2024-10-22 10:30:30 -0300

    Criado um dropdown

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import tkinter
 from tkinter import messagebox as mb
 from PIL import Image, ImageTk
+from tkinter import ttk  # Importar ttk para usar Combobox
 import sqlite3
 
-
+# Conexão com o banco de dados SQLite
 connection = sqlite3.connect('example.db')
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS Tabela1 (nome TEXT, cpf TEXT, estado TEXT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS Tabela1 (nome TEXT, cpf TEXT, estado TEXT, tipo TEXT)")
 
-def inserevalores(nome, cpf, estado):
-    
+def inserevalores(nome, cpf, estado, tipo):
+    # Verifica se o estado está no arquivo config.txt
     with open("config.txt", "r") as file:
         content = file.read()
-        estados = content.split(';')   
-    
+        estados = content.split(';')
+
     if estado not in estados:
         mb.showerror("Erro", "Estado inválido! O estado deve estar contido no arquivo config.txt")
         return
 
-    cursor.execute("INSERT INTO Tabela1 VALUES (?, ?, ?)", (nome, cpf, estado))
+    cursor.execute("INSERT INTO Tabela1 VALUES (?, ?, ?, ?)", (nome, cpf, estado, tipo))
     connection.commit()
     mb.showinfo("Sucesso", f"Dados de '{nome}' inseridos com sucesso!")
 
@@ -27,9 +28,11 @@ def Main():
     root.title("Trabalho RAD")
     root.resizable(False, False)
 
+    # Carregue a imagem usando Pillow
     image = Image.open(r"C:\Users\Laris\OneDrive\Área de Trabalho\TrabalhoPy\biru.png")
     bg_image = ImageTk.PhotoImage(image)
 
+    # Usar Canvas para colocar a imagem de fundo
     canvas = tkinter.Canvas(root, width=image.width, height=image.height)
     canvas.pack(fill="both", expand=True)
     canvas.create_image(0, 0, image=bg_image, anchor="nw")
@@ -55,7 +58,15 @@ def Main():
     e3 = tkinter.Entry(frame, textvariable=estado_var)
     e3.pack(pady=5)
 
-    salvar_button = tkinter.Button(frame, text="Salvar", command=lambda: inserevalores(nome_var.get(), cpf_var.get(), estado_var.get()))
+    # Adicionando o Combobox para selecionar o tipo
+    label_tipo = tkinter.Label(frame, text="Tipo:", bg='lightgray')
+    label_tipo.pack(pady=5)
+    tipo_var = tkinter.StringVar()
+    combobox_tipo = ttk.Combobox(frame, textvariable=tipo_var)
+    combobox_tipo['values'] = ('clt', 'mei', 'socio')
+    combobox_tipo.pack(pady=5)
+
+    salvar_button = tkinter.Button(frame, text="Salvar", command=lambda: inserevalores(nome_var.get(), cpf_var.get(), estado_var.get(), tipo_var.get()))
     salvar_button.pack(pady=10)
 
     root.mainloop()
